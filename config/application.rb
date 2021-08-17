6aefadcd1655fbf20825993aed99a830fdf42393
hamza <hafizhamzasaleem4@gmail.com>
2021-08-18 00:37:47 +0500

    domain added

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module CoursesProject
     config.action_mailer.raise_delivery_errors = false
     config.action_mailer.perform_deliveries = true
     config.action_mailer.delivery_method = :smtp
-    config.action_mailer.default_url_options = { host: 'herokuapp.com' }
+    config.action_mailer.default_url_options = { host: 'courses-2020.herokuapp.com', protocol: 'https' }
 
     ActionMailer::Base.smtp_settings = {
       user_name: ENV['SENDGRID_USERNAME'],
