9586023ea0ac020171aa63051d3b2b0ccfa3c72d
hamza <hafizhamzasaleem4@gmail.com>
2021-08-21 01:12:13 +0500

    add

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,7 +1,14 @@
 class ConfirmationsController < Devise::ConfirmationsController
 
-  def after_inactive_sign_up_path_for(resource_or_scope)
-     root_path
-  end
+  def after_confirmation_path_for(resource_name, resource)
+     sign_in(resource)
+     if params[:redirect_url].present?
+     params[:redirect_url]
+   else
+      root_path
+   end
+ end
+
+
 
 end
